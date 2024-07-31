05e637bcbb669d0c1a6b7b5b088dce8c60a94bf2
Ollie Spacey <ogspacey16@gmail.com>
2024-07-31 18:33:17 +0100

    Laid out structure of demographic analysis code, including key sections and actions.

--- Silwood_Mistletoe_Demography.R
+++ Silwood_Mistletoe_Demography.R
@@ -1,3 +1,65 @@
 ### Silwood Mistletoe Demography
 ### Oliver G. Spacey, Owen R. Jones, Sydne Record, Roberto Salguero-Gómez
 ### 31.07.2024
+
+# Pre-amble ---------------------------------------------------------------
+# Clear environment
+rm(list = ls())
+
+# Load packages
+library(ggplot2)   # Data visualisation
+library(tidyverse) # Data wrangling
+
+# Set working directory as required
+
+# Load data ---------------------------------------------------------------
+# Load mistletoe demographic data
+read_csv()
+
+# Load host metadata
+read_csv()
+
+# Wrangle data ------------------------------------------------------------
+# Combine mistletoe demographic data and host data
+
+# Explore data --------------------------------------------------------
+# Visualise distribution of mistletoe sizes
+
+# Convert 
+
+# Visualise distribution of mistletoe heights
+
+# Visualise distribution of host
+
+
+# Assign stage and sex ----------------------------------------------------
+
+# Set cut-off for juveniles and adults
+
+# Assign known females 
+
+
+# Construct and select linear models --------------------------------------------------------
+
+## Model survival ----------------------------------------------------------
+
+
+## Model growth ------------------------------------------------------------
+
+
+
+## Model reproduction ------------------------------------------------------
+
+
+# Construct IPM -----------------------------------------------------------
+
+
+# Analyse IPM -------------------------------------------------------------
+# Calculate asymptotic growth rate
+
+
+# Calculate generation time
+
+
+
+
